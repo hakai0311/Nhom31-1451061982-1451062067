2faf3f6b70c1ac767e145235e93e8793e52b7a54
hakai0311 <you@example.com>
2017-05-24 22:25:20 -0700

    Add new file BaoCao.docx

--- trasuachotamhon.sql
+++ trasuachotamhon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2017 at 06:45 AM
+-- Generation Time: May 25, 2017 at 03:47 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -58,6 +58,26 @@ CREATE TABLE `theloai` (
   `tenTL` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `username` varchar(31) COLLATE utf8_unicode_ci NOT NULL,
+  `pass` varchar(31) COLLATE utf8_unicode_ci NOT NULL,
+  `quyen` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `pass`, `quyen`) VALUES
+(1, 'hakai', 'hakai', 'admin');
+
 --
 -- Indexes for dumped tables
 --
@@ -81,6 +101,12 @@ ALTER TABLE `tacgia`
 --
 ALTER TABLE `theloai`
   ADD PRIMARY KEY (`maTL`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

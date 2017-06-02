1132b447b8e249402bc15d5e4df4cc8abd0f411b
Your Name <you@example.com>
2017-06-02 20:30:11 +0700

    change 2/6/2017

--- trasuachotamhon.sql
+++ trasuachotamhon.sql
@@ -2,10 +2,10 @@
 -- version 4.6.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 25, 2017 at 03:47 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 02, 2017 lúc 12:38 CH
+-- Phiên bản máy phục vụ: 5.7.14
+-- Phiên bản PHP: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,96 +17,152 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `trasuachotamhon`
+-- Cơ sở dữ liệu: `trasuachotamhon`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `baiviet`
+-- Cấu trúc bảng cho bảng `baiviet`
 --
 
 CREATE TABLE `baiviet` (
-  `maBviet` int(10) NOT NULL,
-  `maTG` int(10) NOT NULL,
-  `maTL` int(10) NOT NULL,
+  `maBV` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
+  `maDM` int(10) NOT NULL,
   `tieuDe` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `tomTat` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `noiDung` varchar(8000) COLLATE utf8_unicode_ci NOT NULL,
+  `hinhAnh` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `ngayDang` date NOT NULL,
-  `hinhAnh` longblob NOT NULL
+  `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tacgia`
+-- Cấu trúc bảng cho bảng `danhmucbaiviet`
+--
+
+CREATE TABLE `danhmucbaiviet` (
+  `maDM` int(10) NOT NULL,
+  `tenDM` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `danhmucbaiviet`
+--
+
+INSERT INTO `danhmucbaiviet` (`maDM`, `tenDM`) VALUES
+(1, 'Truyện ngắn');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tacgia`
 --
 
 CREATE TABLE `tacgia` (
   `maTG` int(10) NOT NULL,
-  `tenTG` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+  `tenTG` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tacgia`
+--
+
+INSERT INTO `tacgia` (`maTG`, `tenTG`, `email`) VALUES
+(1, 'hakai', 'xuanham01234@gmail.com');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `theloai`
---
-
-CREATE TABLE `theloai` (
-  `maTL` int(10) NOT NULL,
-  `tenTL` varchar(50) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
+-- Cấu trúc bảng cho bảng `user`
 --
 
 CREATE TABLE `user` (
   `id` int(10) NOT NULL,
-  `username` varchar(31) COLLATE utf8_unicode_ci NOT NULL,
-  `pass` varchar(31) COLLATE utf8_unicode_ci NOT NULL,
-  `quyen` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `pass` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `quyen` varchar(10) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `user`
+-- Đang đổ dữ liệu cho bảng `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `pass`, `quyen`) VALUES
-(1, 'hakai', 'hakai', 'admin');
+INSERT INTO `user` (`id`, `username`, `pass`, `email`, `quyen`) VALUES
+(2, 'hakai', '3dedaea9655effaf3305dbe0b5bda488', 'xuanham01234@gmail.com', 'admin'),
+(3, 'phamha', 'phamha', '', 'admin'),
+(4, '12345', '827ccb0eea8a706c4c34a16891f84e7b', 'jdkalsdka', 'dsdkld'),
+(5, 'thuy', '3cf2b6b121d1726bc2cdc88c39e0b119', 'hapt42@wru.vn', 'admin');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `baiviet`
+-- Chỉ mục cho bảng `baiviet`
 --
 ALTER TABLE `baiviet`
-  ADD PRIMARY KEY (`maBviet`),
-  ADD KEY `maTG` (`maTG`),
-  ADD KEY `maTL` (`maTL`);
+  ADD PRIMARY KEY (`maBV`),
+  ADD KEY `maTG` (`id_user`),
+  ADD KEY `maDM` (`maDM`);
 
 --
--- Indexes for table `tacgia`
+-- Chỉ mục cho bảng `danhmucbaiviet`
+--
+ALTER TABLE `danhmucbaiviet`
+  ADD PRIMARY KEY (`maDM`);
+
+--
+-- Chỉ mục cho bảng `tacgia`
 --
 ALTER TABLE `tacgia`
   ADD PRIMARY KEY (`maTG`);
 
 --
--- Indexes for table `theloai`
---
-ALTER TABLE `theloai`
-  ADD PRIMARY KEY (`maTL`);
-
---
--- Indexes for table `user`
+-- Chỉ mục cho bảng `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `baiviet`
+--
+ALTER TABLE `baiviet`
+  MODIFY `maBV` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT cho bảng `danhmucbaiviet`
+--
+ALTER TABLE `danhmucbaiviet`
+  MODIFY `maDM` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT cho bảng `tacgia`
+--
+ALTER TABLE `tacgia`
+  MODIFY `maTG` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT cho bảng `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `baiviet`
+--
+ALTER TABLE `baiviet`
+  ADD CONSTRAINT `baiviet_ibfk_1` FOREIGN KEY (`maDM`) REFERENCES `danhmucbaiviet` (`maDM`) ON DELETE CASCADE,
+  ADD CONSTRAINT `baiviet_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
